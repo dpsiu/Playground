b68d7ccb50767daeabb0a668be526f7bfaa4297f
dpsiu <denversiu24@gmail.com>
2023-06-19 18:11:28 -0500

    add feature - image slider JS

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ window.onclick = function(event) {
       }
     }
   }
+
+//   ---------------------- Card 2: Image Carousel ----------------------
+
+let prev = document.querySelector('prev')
+let next = document.querySelector('next')
+
+let slideIndex = 1;
+showSlides(slideIndex)
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    console.log('prev clicked')
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    let slide = document.getElementsByClassName('picture')
+    console.log(slide.length)
+    if (n > slide.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slide.length}
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none"
+    }
+
+    slide[slideIndex-1].style.display = "block";
+
+}
