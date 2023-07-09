6d4d70c844440536fcce923c808b00b1e851ddd9
dpsiu <denversiu24@gmail.com>
2023-07-09 17:17:48 -0500

    Add feature - mergesort function for practice
    A mergesort function was added for the sake of practicing the
    algorithm and application

--- script.js
+++ script.js
@@ -89,3 +89,91 @@ function productOfArray(array){
 
 // return in this case had to get a number, then calculate the slightly changed 2nd num
 
+function fibs(n) {
+  let array = [0, 1]
+  let n1 = 0
+  let n2 = 1
+
+  if (n == 0 || n == 1) return array
+
+  for (i = 0; i <= n-3; i++) {
+    let n3 = n2 + n1
+    array.push(n3)
+
+    n1 = n2
+    n2 = n3
+  }
+  console.log(array)
+}
+
+function fibsRec(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    const sequence = fibsRec(n - 1);
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    return sequence;
+  }
+}
+
+
+function mergeSort (array) {
+  if (array.length <= 1) {
+    return array
+  }
+
+  const middle = Math.floor(array.length/2)
+  const left = array.slice(0, middle)
+  const right = array.slice(middle)
+
+  const sortedLeft = mergeSort(left)
+  const sortedRight = mergeSort(right)
+
+  console.log(sortedLeft)
+  console.log(sortedRight)
+
+  return merge(sortedLeft, sortedRight)
+}
+
+function merge (left, right) {
+  result = []
+  leftIndex = 0
+  rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex])
+      leftIndex ++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex ++
+    }
+  }
+
+  result = result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+
+  return result
+}
+
+
+
+// Think what are my cases and hwo I'd go about it.
+
+// First, base case. When array is <= 1.
+
+// We need to lay out the rules for how to break halves.
+// left, middle, right. Take our array, slice into sep arrays, continue down 
+//    until each array has 1 element. Keep going if index of left is lower than
+//    index of right. BC this means 2 items. If only 1, then they are the same.
+
+// This is first half done. Breaking the array into tiny ones. Now we merge and sort.
+// In pseudo-code, we compare left and right halves and set left and right indexes to 0
+//    (How do we identify which parts are left and right? Are these just the return?)
+//    Seems like they are defined as left and right from the beginning. 
+// L is 0, middle, and R is middle onwards. BC it is recursively recalled, they are
+//  continuously broken down. Starts down up. L and R are only one thing. THat which
+//    is being compared.
